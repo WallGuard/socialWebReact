901fbed716ef2759ee9e28982610e994a214976c
Mikhail <mikhail-aria@yandex.ru>
2020-08-10 14:54:15 +0300

    adding classes

--- src/PAGES/Users/index.jsx
+++ src/PAGES/Users/index.jsx
@@ -3,54 +3,62 @@ import * as axios from 'axios';
 const malePic = 'https://toppng.com/uploads/preview/login-icons-user-flat-icon-115534363917nmr24mjcm.png';
 const femalePic = 'https://www.pinclipart.com/picdir/middle/164-1640717_free-user-icon-flat-189024-download-user-icon.png';
 
-const Users = (props) => {
-    const getUSers = () => {
-    props.users.length === 0 &&
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+        this.getUsers()
+    };
+
+    getUsers = () => {
         axios.get('http://localhost:4000/api/users')
             .then(response => {
-                debugger;
-                props.setUsers(response.data)
+                this.props.setUsers(response.data)
         });
-    }
-
-    const validatePic = (gender) => {
+    };
+    
+    validatePic = (gender) => {
         return gender === 'male' ? malePic : femalePic
     };
 
-    return (
-        <div>
-            <button onClick={getUSers}>Get Users</button>
-            {
-                props.users.map(u => 
-                    <div key={u.id}>
-                        <div>
-                        <div>
-                            <img
-                                src={u.img === 'no' || undefined || null ?
-                                    validatePic(u.gender)
-                                    : u.img}
-                                width='100px'
-                                alt='avatar' />
-                        </div>
-                            { !u.followed ?
-                                <button onClick={() => props.follow(u.id)}>Follow</button>
-                                :
-                                <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                            }
-                        </div>
-                        <div>
-                            <div>{u.fullName}</div>
-                            <div>{u.status}</div>
+    onFollowClick = (id) => this.props.follow(id);
+    onUnfollowClick = (id) => this.props.unfollow(id);
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.getUsers}>Get Users</button>
+                {
+                    this.props.users.map(u => 
+                        <div key={u.id}>
                             <div>
-                            <span>{u.location.city}, </span>
-                            <span>{u.location.country}</span>
+                            <div>
+                                <img
+                                    src={u.img === 'no' || undefined || null ?
+                                        this.validatePic(u.gender)
+                                        : u.img}
+                                    width='100px'
+                                    alt='avatar' />
+                            </div>
+                                { !u.followed ?
+                                    <button onClick={() => this.onFollowClick(u.id)}>Follow</button>
+                                    :
+                                    <button onClick={() => this.onUnfollowClick(u.id)}>Unfollow</button>
+                                }
+                            </div>
+                            <div>
+                                <div>{u.fullName}</div>
+                                <div>{u.status}</div>
+                                <div>
+                                <span>{u.location.city}, </span>
+                                <span>{u.location.country}</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            }
-        </div>
-    )
-};
+                    )
+                }
+            </div>
+        )
+    }
+} 
 
 export default Users;
